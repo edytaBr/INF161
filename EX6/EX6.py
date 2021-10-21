638b860e0815c31aaccfc303679b7e634f3368a4
edytaBr <ebredla@gmail.com>
2021-10-21 15:40:37 +0200

    add ex6: done

--- EX6/EX6.py
+++ EX6/EX6.py
@@ -70,48 +70,93 @@ data = pd.read_csv('diabetes.csv')
 X = data.drop('Outcome', axis=1)
 Y = data['Outcome']
 # Split data
-X_train, X_rem, y_train, y_rem = train_test_split(X, Y, train_size=0.6)
-test_size = 0.5
-X_valid, X_test, y_valid, y_test = train_test_split(
-    X_rem, y_rem, test_size=0.5, random_state=20)
+
 
 knn = KNeighborsClassifier()
 
+imputers = ["mean", "median", "most_frequent"]
 
-#mean
-imp_mean = SimpleImputer(missing_values=np.NaN, strategy='mean')
-imp_mean.fit(X_train)
+def simpleImp(a): 
+    imp = SimpleImputer(missing_values=np.NaN, strategy=a)
+    imp.fit(X_train)
 
-X_train_mean_imp = imp_mean.transform(X_train)
-X_val_mean_imp = imp_mean.transform(X_valid)
+    X_train_imp = imp.transform(X_train)
+    X_val_imp = imp.transform(X_valid)
 
-knn.fit(X_train_mean_imp, y_train)
-y_val_mean = knn.predict(X_val_mean_imp)
-print(accuracy_score(y_valid, y_val_mean))
+    knn.fit(X_train_imp, y_train)
+    y_val = knn.predict(X_val_imp)
+    print(a , accuracy_score(y_valid, y_val))
 
-
-
-
-#median
-imp_median = SimpleImputer(missing_values=np.NaN, strategy='median')
-imp_median.fit(X_train)
-
-X_train_median_imp = imp_median.transform(X_train)
-X_val_median_imp = imp_median.transform(X_valid)
-
-knn.fit(X_train_median_imp, y_train)
-y_val_median = knn.predict(X_val_median_imp)
-print(accuracy_score(y_valid, y_val_median))
+    
 
 
+for imput in imputers:
+    X_train, X_rem, y_train, y_rem = train_test_split(X, Y, train_size=0.6)
+    test_size = 0.5
+    X_valid, X_test, y_valid, y_test = train_test_split(
+    X_rem, y_rem, test_size=0.5, random_state=20)
+    simpleImp(imput)
 
-#most_frequent
-imp_mf = SimpleImputer(missing_values=np.NaN, strategy='most_frequent')
-imp_mf.fit(X_train)
+# I run this code several times. The mean SimpleImputer strategy had the best (highest) accurancy score. Thus following:
+simpleImp("mean")
 
-X_train_mf_imp = imp_mf.transform(X_train)
-X_val_mf_imp = imp_mf.transform(X_valid)
 
-knn.fit(X_train_mf_imp, y_train)
-y_val_mf = knn.predict(X_val_mf_imp)
-print(accuracy_score(y_valid, y_val_mf))
+
+#%%
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('titanic.csv')
+data["Title"] = data['Name'].str.extract(r'([A-Za-z]+)\.')
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, Y, train_size=0.2)
+X = data.drop('Survived', axis=1)
+Y = data['Survived']
+
+
+data["Title"].unique()
+data['Title'] = data['Title'].replace(['Dr', 'Rev', 'Col', 'Major', 'Countess', 'Sir', 'Jonkheer', 'Lady', 'Capt', 'Don'], 'Others')
+
+#Fix typos
+data['Title'] = data['Title'].replace('Ms', 'Miss')
+data['Title'] = data['Title'].replace('Mme', 'Mrs')
+data['Title'] = data['Title'].replace('Mlle', 'Miss')
+
+data[['Title', 'Survived']].groupby('Title').mean()
+
+
+group = X_train['Sex'].groupby(X_train["Title"]).count()
+df = group.reset_index()
+df = df.sort_values(by=['Sex'], ascending=False)
+ax = sns.barplot( df.Title, df.Sex)
+ax.set_title('Titles in training set')
+ax.set_xticklabels(ax.get_xticklabels(),rotation = 90)
+ax.set_title('Titles in training set')
+
+
+df2 = data[['Title', 'Survived']].groupby('Title').mean()
+df2 = df2.reset_index()
+ax2 = sns.barplot( df2.Title, df2.Survived)
+ax2.set_title('Titles in training set')
+ax2.set_xticklabels(ax.get_xticklabels(),rotation = 90)
+
+
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, Y, train_size=0.8)
+X = data.drop('Survived', axis=1)
+Y = data['Survived']
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+title_encoded = le.fit_transform(data['Title'])
+data['encoded_title'] = title_encoded
+
+X_train, X_test, y_train, y_test = train_test_split(data['Fare'], data['Survived'], test_size=.2, random_state=20)
+knn.fit(X_train.to_numpy().reshape(-1,1), y_train.to_numpy().reshape(-1,1))
+accuracy_score(y_test, knn.predict(X_test.to_numpy().reshape(-1,1)))
+
+X_train, X_test, y_train, y_test = train_test_split(data[['Fare', 'encoded_title']], data['Survived'], test_size=.2, random_state=20)
+knn.fit(X_train.to_numpy().reshape(-1,2), y_train.to_numpy().reshape(-1,1))
+accuracy_score(y_test, knn.predict(X_test.to_numpy().reshape(-1,2)))
