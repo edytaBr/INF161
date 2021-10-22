67362469c3ec3aa34f8dada9337d938b081720dc
edytaBr <ebredla@gmail.com>
2021-10-22 14:23:15 +0200

    add main part of web

--- Project/web/main.py
+++ Project/web/main.py
@@ -7,14 +7,39 @@ Created on Thu Oct 21 11:07:29 2021
 """
 
 from flask import Flask, render_template
+from flask import request
+
 import pandas as pd
 app = Flask(__name__)
 @app.route("/")
 
 def template():
-   data = pd.read_csv("results.csv") 
-   myData = data.values
-   return render_template("template.html", myData=myData)
+
+   return render_template("template.html")
+
+
+
+
+@app.route('/', methods=['POST'])
+def score_send():
+    data = pd.read_csv("results.csv") 
+
+    h = request.form['home']
+    a = request.form['away']
+    model = request.form['model']
+    
+    for elem in data.itertuples():
+        if ((model == 'knn') and ((elem.Home == h) and (elem.Away == a))):
+            score_Home = elem.HomeScore_KN
+            score_Away = elem.AwayScore_KN
+        elif ((model == 'randomforest') and ((elem.Home == h) and (elem.Away == a))):
+            score_Home = elem.HomeScore_RF
+            score_Away = elem.AwayScore_RF
+            
+    return render_template("predict.html", myData = [ score_Home, score_Away])
+
+
+
 
 
 if __name__ == "__main__":
