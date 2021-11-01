2f6c992aaa0febdefc8f2c96705e1d59d7d8f7ed
edytaBr <ebredla@gmail.com>
2021-11-01 13:45:27 +0100

    add exercise 1 and 2 - EX7

---
+++ EX7/EX7.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  1 11:40:27 2021
+
+@author: edyta
+"""
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+ratings = pd.read_csv('ratings2.csv')
+films = pd.read_csv('films.csv')
+item = pd.read_csv('item_based.csv',  decimal=",")
+coll = ratings - ratings.mean()
+
+corr = coll.corr
